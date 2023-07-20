13d67997bb2557b91729e93560950b08c53bf182
Soham Govande <vgsoham@gmail.com>
2023-07-20 12:25:37 -0700

    Initial commit

---
+++ index.test.ts
@@ -0,0 +1,74 @@
+import * as schema from './index'
+import {config} from 'dotenv'
+
+config()
+
+describe('booleans', () => {
+  it("should return true for a positive review", () => expect(schema.binary("Did this review user like the business? Best bang for your buck. For a price much cheaper than college consultants, I have hundreds of successful Ivy League applications at my fingertips.")).resolves.toBe(true))
+
+  it("should handle short strings", () => expect(schema.binary("Did this review user like the business? worst service EVER!")).resolves.toBe(false))
+
+  it("should return false for an empty string", () => expect(schema.binary("")).resolves.toBe(false))
+})
+
+describe('enums', () => {
+  it("should classify basic values correctly", () => expect(schema.categorize("My favorite color is red", ["red", "blue", "green"])).resolves.toBe("red"))
+
+  it("should not throw an error for an invalid value", () => expect(schema.categorize("My favorite color is red", ["blue", "green"])).resolves.not.toThrow())
+
+  it("should classify values with spaces correctly", () => expect(schema.categorize("I prefer watching Science Fiction movies", ["Science Fiction", "Romantic Comedy", "Action Adventure"])).resolves.toBe("Science Fiction"))
+
+  it("should classify values with special characters correctly", () => expect(schema.categorize("I enjoy listening to Guns N' Roses", ["AC'DC", "Guns N' Roses", "Léd Zeppelin"])).resolves.toBe("Guns N' Roses"))
+})
+
+describe('createList function', () => {
+  it("should classify multiple basic values correctly", async () => {
+    const values = await schema.list("My favorite colors are red and green", ["red", "blue", "green"])
+    expect(values).toEqual(["red", "green"])
+  })
+
+  it("should handle a mix of valid and invalid values", async () => {
+    const values = await schema.list("My favorite colors are red, green and pink", ["red", "blue", "green"])
+    expect(values).toEqual(["red", "green"])
+  })
+
+  it("should classify multiple values with spaces correctly", async () => {
+    const values = await schema.list("I enjoy watching Science Fiction and Action Adventure movies", ["Science Fiction", "Romantic Comedy", "Action Adventure"])
+    expect(values).toEqual(["Science Fiction", "Action Adventure"])
+  })
+
+  it("should classify multiple values with special characters correctly", async () => {
+    const values = await schema.list("I enjoy listening to AC/DC and Guns N' Roses", ["AC/DC", "Guns N' Roses", "Led Zeppelin"])
+    expect(values).toEqual(["AC/DC", "Guns N' Roses"])
+  })
+
+  it("should limit the returned values to the specified maxValues", async () => {
+    const values = await schema.list("I enjoy listening to AC/DC, Guns N' Roses, and Led Zeppelin", ["AC/DC", "Guns N' Roses", "Led Zeppelin"], 1, 2)
+    expect(values.length).toBe(2)
+  })
+
+  it("should throw an error when fewer than minValues are provided", async () => {
+    await expect(schema.list("I enjoy listening to AC/DC", ["AC/DC", "Guns N' Roses", "Led Zeppelin"], 2, 3)).rejects.toThrow()
+  })
+})
+
+
+describe('createList function edge cases', () => {
+  it("should handle the edge case where minValues is equal to maxValues", async () => {
+    await expect(schema.list("I enjoy listening to AC/DC and Guns N' Roses", ["AC/DC", "Guns N' Roses", "Led Zeppelin"], 2, 2)).rejects.toThrow()
+  })
+
+  it("should return an empty array when no values are provided and minValues is zero", async () => {
+    const values = await schema.list(" ", ["AC/DC", "Guns N' Roses", "Led Zeppelin"], 0, 3)
+    expect(values).toEqual([])
+  })
+  it("should not return an empty array when no values are provided and minValues is non zero", async () => {
+    const values = await schema.list(" ", ["AC/DC", "Guns N' Roses", "Led Zeppelin"], 1, 3)
+    expect(values.length).toBeGreaterThan(0)
+  })
+
+  it("should handle the case where the prompt mentions more items than maxValues", async () => {
+    const values = await schema.list("I enjoy listening to AC/DC, Guns N' Roses, Led Zeppelin, and Pink Floyd", ["AC/DC", "Guns N' Roses", "Led Zeppelin", "Pink Floyd"], 1, 3)
+    expect(values.length).toBe(3)
+  })
+})
---
+++ index.ts
@@ -0,0 +1,213 @@
+import {ChatOpenAI} from "langchain/chat_models/openai"
+import {HumanMessage, SystemMessage} from "langchain/schema";
+import {z, type ZodError} from 'zod'
+
+interface GenericPromptOptions {
+  gpt4?: boolean
+}
+
+type AtLeastOne<T> = [T, ...T[]];
+
+function createLLM(promptOptions?: GenericPromptOptions) {
+  return new ChatOpenAI({
+    temperature: 0,
+    modelName: promptOptions?.gpt4 ? "gpt-4" : "gpt-3.5-turbo"
+  })
+}
+
+/**
+ * Asynchronously handles a binary prompt to return a boolean answer.
+ *
+ * This function creates a Language Learning Model (LLM) from the provided options
+ * and prompts the user with a message. It then returns a boolean value based on the
+ * user's answer.
+ *
+ * @export
+ * @param {string} prompt - The prompt message to display to the user.
+ * @param {GenericPromptOptions} [promptOptions] - Optional settings for the prompt.
+ * @returns {Promise<boolean>} A promise that resolves to a boolean indicating the user's response.
+ *
+ * @throws {ZodError} If the parsed response does not match the expected structure.
+ *
+ * @async
+ */
+export async function binary(prompt: string, promptOptions?: GenericPromptOptions): Promise<boolean> {
+  if (!prompt)  {
+    return false
+  }
+  const llm = createLLM(promptOptions)
+  const result = await llm.call([
+    new SystemMessage("Answer the following question with a true or false."),
+    new HumanMessage(prompt)
+  ], {
+    function_call: {name: "answer"},
+    functions: [
+      {
+        name: "answer",
+        description: "Answer the user's question",
+        parameters: {
+          type: "object",
+          required: ["value"],
+          description: "An object containing a boolean value.",
+          properties: {
+            value: {
+              type: "boolean",
+              description: "The boolean value to return.",
+            },
+          },
+        },
+      }
+    ]
+  })
+  const zBooleanAnswer = z.object({value: z.boolean()})
+  const answer = JSON.parse(result.additional_kwargs.function_call?.arguments as string)
+  return zBooleanAnswer.parse(answer).value
+}
+
+/**
+ * Asynchronously handles a categorical prompt and returns the classified category
+ *
+ * This function creates a Language Learning Model (LLM) from the provided options
+ * and prompts the user with a message. It then returns the selected category,
+ * which must be one of the provided allowed values.
+ *
+ * @export
+ * @param {string} prompt - The user's question to classify
+ * @param {AtLeastOne<string>} allowedValues - Array of allowable categorical values.
+ * @param {GenericPromptOptions} [promptOptions] - Optional settings for the prompt.
+ * @returns {Promise<string>} A promise that resolves to a string indicating the user's selected category.
+ *
+ * @throws {Error} If no prompt is provided.
+ * @throws {ZodError} If the parsed response does not match the expected structure or is not one of the allowed values.
+ *
+ * @async
+ */
+export async function categorize(prompt: string, allowedValues: AtLeastOne<string>, promptOptions?: GenericPromptOptions): Promise<string> {
+  if (!prompt) {
+    throw new Error("Prompt is required")
+  }
+  const llm = createLLM(promptOptions)
+  const result = await llm.call([
+    new SystemMessage(`Answer the following question with one of the following allowed values: ${allowedValues.join(", ")}. You MUST use the exact spelling and capitalization of the values.`),
+    new HumanMessage(prompt)
+  ], {
+    function_call: {name: "answer"},
+    functions: [
+      {
+        name: "answer",
+        description: "Answer the user's question",
+        parameters: {
+          type: "object",
+          required: ["value"],
+          properties: {
+            value: {
+              type: "string",
+              enum: allowedValues,
+              description: "The value to use, MUST be one of the allowed values",
+            },
+          },
+        },
+      }
+    ]
+  })
+  const returnedValue = JSON.parse(result.additional_kwargs.function_call?.arguments as string)
+  const zStringAnswer = z.object({value: z.enum(allowedValues)})
+  return zStringAnswer.parse(returnedValue).value
+}
+
+/**
+ * Asynchronously handles a list prompt and returns an array of selected values.
+ *
+ * This function creates a Language Learning Model (LLM) from the provided options
+ * and prompts the user with a message. The user is expected to select a minimum
+ * and maximum number of values from the allowed list, and the function returns an array
+ * of these values.
+ *
+ * @export
+ * @param {string} prompt - The prompt message to display to the user.
+ * @param {AtLeastOne<string>} allowedValues - Array of allowable values.
+ * @param {number} [minValues=1] - The minimum number of values the user must select.
+ * @param {number} [maxValues=5] - The maximum number of values the user can select.
+ * @param {GenericPromptOptions} [promptOptions] - Optional settings for the prompt.
+ * @returns {Promise<string[]>} A promise that resolves to an array of strings indicating the user's selected values.
+ *
+ * @throws {Error} If no prompt is provided, if minValues is not less than maxValues, or if minValues is not greater than zero.
+ * @throws {ZodError} If the parsed response does not match the expected structure or is not one of the allowed values.
+ *
+ * @async
+ */
+export async function list(prompt: string, allowedValues: AtLeastOne<string>, minValues = 1, maxValues = 5, promptOptions?: GenericPromptOptions): Promise<string[]> {
+  if (minValues >= maxValues) {
+    throw new Error("minValues must be less than maxValues")
+  }
+  if (minValues < 0) {
+    throw new Error("minValues must be greater than zero")
+  }
+  if (!prompt) {
+    return []
+  }
+  const llm = createLLM(promptOptions)
+  const zeroMessage = minValues === 0 ? "You may also answer with no values." : ""
+  const multipleMessage = minValues > 1 ? "You may also answer with multiple values." : ""
+  const result = await llm.call([
+    new SystemMessage(`Answer the following question with AT LEAST ${minValues} and AT MOST ${maxValues} of the following allowed values: ${allowedValues.join(", ")}. You MUST use the exact spelling and capitalization of the values. ${multipleMessage}${zeroMessage}`),
+    new HumanMessage(prompt)
+  ], {
+    function_call: {name: "answer"},
+    functions: [
+      {
+        name: "answer",
+        description: "Answer the user's question",
+        parameters: {
+          type: "object",
+          required: ["value"],
+          properties: {
+            value: {
+              type: "array",
+              description: "The values to use, MUST be from the list of allowed values",
+              minItems: minValues,
+              maxItems: maxValues,
+              items: {
+                type: "string",
+                enum: allowedValues
+              }
+            },
+          },
+        },
+      }
+    ]
+  })
+  const returnedValue = JSON.parse(result.additional_kwargs.function_call?.arguments as string)
+  const zStringArrayAnswer = z.object({
+    value: z.array(z.enum(allowedValues))
+  })
+  const parsedValue = zStringArrayAnswer.parse(returnedValue).value
+  if (parsedValue.length < minValues) {
+    throw new Error(`You must provide at least ${minValues} values`)
+  } else if (parsedValue.length > maxValues) {
+    parsedValue.splice(maxValues)
+  }
+  return parsedValue
+}
+
+/**
+ * Asynchronously handles a string prompt and returns any string response. No ouptut validation is performed.
+ *
+ * @export
+ * @param {string} prompt - The prompt message to display to the user.
+ * @returns {Promise<string>} A promise that resolves to a string indicating the content of the user's response.
+ *
+ * @throws {Error} If the LLM fails to create or call.
+ *
+ * @async
+ */
+export async function string(prompt: string) {
+  if (!prompt) {
+    return ""
+  }
+  const llm = createLLM()
+  const result = await llm.call([
+    new HumanMessage(prompt)
+  ])
+  return result.content
+}
